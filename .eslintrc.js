2c3cae33c0fa60da470ca9987154312eb8328103
Joe Gregorio <jcgregorio@google.com>
2022-03-08 12:21:05 -0500

    Convert build.js to build.ts.

---
+++ .eslintrc.js
@@ -0,0 +1,125 @@
+// To make use of this eslint config file run:
+//
+//   npm ci
+//
+// Then install eslint support in your IDE of choice.
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['airbnb-base'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  ignorePatterns: ['dist/', 'build/', '_bazel*'],
+  rules: {
+    camelcase: ['off'],
+    'class-methods-use-this': ['off'],
+    'func-names': ['off'],
+    'import/prefer-default-export': ['off'],
+    'max-classes-per-file': ['off'],
+    'max-len': ['off'],
+    'no-alert': ['off'],
+    'no-bitwise': ['warn'],
+    'no-continue': ['off'],
+    'no-lone-blocks': ['off'],
+    'no-param-reassign': ['off'],
+    'no-plusplus': ['off'],
+    'no-restricted-syntax': ['warn'],
+    'no-return-assign': ['off'],
+    'no-shadow': ['warn'],
+    'no-underscore-dangle': ['off'],
+    'no-use-before-define': ['error', { functions: false, variables: false }],
+    'object-shorthand': ['off'],
+    'prefer-destructuring': ['off'],
+    'prefer-object-spread': ['off'],
+    'space-before-function-paren': [
+      'error',
+      { anonymous: 'never', named: 'never', asyncArrow: 'always' },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+
+      // Start with the recommended rules, but turn some of them off in the
+      // 'rules' section below.
+      extends: ['plugin:@typescript-eslint/recommended'],
+      settings: {
+        'import/resolver': {
+          node: {
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
+          },
+        },
+      },
+      plugins: ['@typescript-eslint'],
+      rules: {
+        // Allow ! non-null assertions.
+        '@typescript-eslint/no-non-null-assertion': 'off',
+
+        // Require a consistent member declaration order
+        '@typescript-eslint/member-ordering': 'off',
+
+        // Don't require the .ts extension for imports.
+        'import/extensions': 'off',
+
+        // Sometimes we need to import an interface, but also we need the
+        // side-effects to run, e.g. to register an element, which requires two
+        // import statements.
+        //
+        // https://github.com/Microsoft/TypeScript/wiki/FAQ#why-are-imports-being-elided-in-my-emit
+        // https://github.com/microsoft/TypeScript/issues/9191
+        'import/no-duplicates': 'off',
+
+        // a: string = 'foo' might be redundant, but it's not harmful.
+        '@typescript-eslint/no-inferrable-types': 'off',
+
+        '@typescript-eslint/type-annotation-spacing': [
+          'error',
+          {
+            before: false,
+            after: true,
+          },
+        ],
+
+        // note you must disable the base rule as it can report incorrect errors
+        'lines-between-class-members': 'off',
+        '@typescript-eslint/lines-between-class-members': [
+          'error',
+          'always',
+          { exceptAfterOverload: true },
+        ],
+
+        // note you must disable the base rule as it can report incorrect errors
+        'space-before-function-paren': 'off',
+        '@typescript-eslint/space-before-function-paren': [
+          'error',
+          {
+            anonymous: 'never',
+            named: 'never',
+            asyncArrow: 'always',
+          },
+        ],
+
+        // We already disallow implicit-any, explicit is fine.
+        '@typescript-eslint/no-explicit-any': 'off',
+      },
+    },
+    {
+      files: ['*_test.ts'],
+      rules: {
+        // Prevents Chai.js assertions such as expect(foo).to.be.true from causing "Expected an
+        // assignment or function call and instead saw an expression." linting errors.
+        'no-unused-expressions': 'off',
+      },
+    },
+  ],
+};
---
+++ build.ts
@@ -0,0 +1,137 @@
+// Convert the files in the /json directory into a full caniuse type website in
+// the /site directory.
+
+import {
+  readdir, writeFile, readFile, copyFile, mkdir,
+} from 'fs/promises';
+import { join, extname } from 'path';
+
+const sourceDataDir = './data';
+const destDir = './site';
+
+interface Link {
+  url: string
+  title: string
+}
+
+type YesNo = 'yes' | 'no'
+
+type Product = { [key: string]: YesNo}
+
+/** The format of the CanIUse data. */
+interface CanIUseData {
+  title: string
+  description: string
+  spec: string
+  links: Link[]
+  categories: string[],
+  stats: {[key: string]: Product}
+  notes: string
+  parent: string
+  keywords: string
+
+  // TODO(jcgregorio) Figure out the shape of the following:
+  bugs: any[]
+  notes_by_num: any,
+}
+
+const loadFile = async (filename: string): Promise<CanIUseData> => {
+  const buf = await readFile(join(sourceDataDir, filename));
+  return JSON.parse(buf.toString());
+};
+
+const htmlFilenameFromJSONFilename = (jsonFilename: string): string => {
+  const rootFileName = jsonFilename.slice(
+    0,
+    jsonFilename.length - extname(jsonFilename).length,
+  );
+
+  return `${rootFileName}.html`;
+};
+
+/** CorpusPage has data for a single page. */
+interface CorpusPage {
+  // Relative URL of the page.
+  url: string
+
+  /** The page title. */
+  title: string
+
+  /** A stripped down CanIUseData serialized as JSON. */
+  content: string
+}
+
+const createCombinedJSONFile = async (sourceDirListing: string[]) => {
+  // First create the combined output file.
+  const combined: CorpusPage[] = [];
+  const wait = sourceDirListing.map(async (filename) => {
+    const parsed: CanIUseData = await loadFile(filename);
+
+    // Remove info we don't want to search on.
+    delete parsed.stats;
+
+    combined.push({
+      url: htmlFilenameFromJSONFilename(filename),
+      title: parsed.title,
+      content: JSON.stringify(parsed),
+    });
+  });
+  await Promise.all(wait);
+
+  await writeFile(join(destDir, 'allData.json'), JSON.stringify(combined));
+};
+
+const expandTemplate = (data: CanIUseData) => `
+<!DOCTYPE html>
+<html>
+<head>
+    <title>${data.title}</title>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<section>
+    <h1><a href="${data.spec}">${data.title}</a></h1>
+    <p>${data.description}</p>
+</section>
+<section>
+    <h2>Notes</h2>
+    <p>${data.notes}</p>
+</section>
+</body>
+</html>
+`;
+
+const createPageForEachDataFile = async (sourceDirListing: string[]): Promise<void> => {
+  const wait = sourceDirListing.map(async (filename) => {
+    const data = await loadFile(filename);
+    const page = expandTemplate(data);
+
+    await writeFile(
+      join(destDir, htmlFilenameFromJSONFilename(filename)),
+      page,
+    );
+  });
+  Promise.all(wait);
+};
+
+const copyOverFixedFiles = async (): Promise<void> => {
+  await copyFile('./index.html', join(destDir, 'index.html'));
+  await copyFile('./index.js', join(destDir, 'index.js'));
+};
+
+const createTargetDir = async (): Promise<void> => {
+  await mkdir(destDir, { mode: 0o755, recursive: true });
+};
+
+const main = async () => {
+  const sourceDirListing = await readdir(sourceDataDir);
+
+  await createTargetDir();
+  await createPageForEachDataFile(sourceDirListing);
+  await createCombinedJSONFile(sourceDirListing);
+  await copyOverFixedFiles();
+};
+
+main();
