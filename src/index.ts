d8ac2c2d6ea68737ae55dffcc1c055b01c4f2bc2
Joe Gregorio <jcgregorio@google.com>
2022-03-09 16:06:44 -0500

    git squash commit for main.
    cb45dda76f8b71ef73635eb27f01c66f01f11219
    custom element works.
    
    2882d8e757744464bcae4589168ab9f1fad32f12
    Add <search-widget> to every page.

--- src/index.ts
+++ src/index.ts
@@ -1,79 +1,86 @@
 import '../css/index.scss';
 
-import { html, render, TemplateResult } from 'lit-html';
+import {
+  html, render, TemplateResult,
+} from 'lit';
 import { CorpusPage } from './common';
 
-const main = async () => {
-  const AUTO_SUBMIT_TIMOUT_MS = 1000;
-  const MAX_RESULTS = 30;
+const AUTO_SUBMIT_TIMOUT_MS = 1000;
+const MAX_RESULTS = 30;
 
-  const searchInput = document.querySelector<HTMLInputElement>('#search_input')!;
-  const results = document.querySelector<HTMLUListElement>('#results')!;
-  const button = document.querySelector<HTMLInputElement>('#submit')!;
-  const form = document.querySelector<HTMLFormElement>('#form')!;
-
-  /** The handle of a timer that will trigger searching. Value of 0 if there is
-   * no pending timer. */
-  let autoSubmitTimer: number = 0;
-
-  // Load the search corpus.
+const loadCorpus = async (): Promise<CorpusPage[]> => {
   const resp = await fetch('./allData.json');
-  const corpus: CorpusPage[] = await resp.json();
+  return resp.json();
+};
 
-  const template = (page: CorpusPage): TemplateResult => html`
-    <li><a href="${page.url}">${page.title}</a></li>
-  `;
+class SearchWidget extends HTMLElement {
+  static corpusPromise: Promise<CorpusPage[]> = loadCorpus();
 
-  /** Does the actual search work and populates the results. */
-  const doSearch = () => {
-    const searchValue: string = searchInput.value;
-    const templateResults: TemplateResult[] = [];
+  private autoSubmitTimer: number = 0;
+
+  private input: HTMLInputElement | null = null;
+
+  private matchingPages: CorpusPage[] = [];
+
+  static template = (ele: SearchWidget): TemplateResult => html`
+    <form id=form @submit=${ele.submitForm}>
+      <input type=text id=search_input @input=${ele.textInput} />
+      <input type=submit value=Query />
+    </form>
+    <ul>
+      ${ele.matchingPages.map((page: CorpusPage): TemplateResult => html`
+        <li><a href="${page.url}">${page.title}</a></li>
+      `)}
+    </ul>`;
+
+  connectedCallback(): void {
+    this.render();
+    this.input = this.querySelector('#search_input');
+  }
+
+  render() {
+    render(SearchWidget.template(this), this, { host: this });
+  }
+
+  submitForm(e: SubmitEvent) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    this.doSearch();
+  }
+
+  textInput() {
+    if (this.autoSubmitTimer) {
+      window.clearTimeout(this.autoSubmitTimer);
+      this.autoSubmitTimer = 0;
+    }
+    this.autoSubmitTimer = window.setTimeout(() => this.doSearch(), AUTO_SUBMIT_TIMOUT_MS);
+  }
+
+  async doSearch() {
+    const searchValue: string = this.input!.value;
     let totalResults: number = 0;
 
+    const corpus: CorpusPage[] = await SearchWidget.corpusPromise;
+    this.matchingPages = [];
     corpus.forEach((page) => {
       if (totalResults > MAX_RESULTS) {
         return;
       }
       if (page.content.includes(searchValue)) {
-        templateResults.push(template(page));
+        this.matchingPages.push(page);
         totalResults++;
       }
     });
-    render(templateResults, results);
+
+    this.render();
 
     // Clear any pending search.
-    if (autoSubmitTimer) {
-      window.clearTimeout(autoSubmitTimer);
-      autoSubmitTimer = 0;
+    if (this.autoSubmitTimer) {
+      window.clearTimeout(this.autoSubmitTimer);
+      this.autoSubmitTimer = 0;
     }
-  };
-
-  // Hook up the query button.
-  form.addEventListener('submit', (e: SubmitEvent) => {
-    e.stopPropagation();
-    e.preventDefault();
-
-    doSearch();
-  });
-
-  // Watch as new characters are entered into the input and
-  // AUTO_SUBMIT_TIMOUT_MS after the last character is entered then trigger the
-  // search w/o the need for the user to press enter.
-  searchInput.addEventListener('input', () => {
-    if (autoSubmitTimer) {
-      window.clearTimeout(autoSubmitTimer);
-      autoSubmitTimer = 0;
-    }
-    autoSubmitTimer = window.setTimeout(doSearch, AUTO_SUBMIT_TIMOUT_MS);
-  });
-
-  // Enable the query button now that all setup is complete.
-  button.disabled = false;
-};
-
-// Only run our code once the DOM is loaded.
-if (document.readyState === 'complete') {
-  main();
-} else {
-  window.addEventListener('DOMContentLoaded', main);
+  }
 }
+
+customElements.define('search-widget', SearchWidget);
