4ff879eeda71493039424f1a43e375886edc755e
airnan <hernantorrisi@gmail.com>
2022-10-04 20:20:57 -0400

    change issue name to New Version for tests

--- run_tests/run.ts
+++ run_tests/run.ts
@@ -1,20 +1,16 @@
-import * as core from '@actions/core';
 import * as github from '@actions/github';
 
 async function run() {
   try {
     console.log('Running test');
-    // const token = 'a';
     const secretToken = process.env.BODYMOVIN_PERSONAL_TOKEN || 'none secret';
-    console.log('secretToken', secretToken.length);
     const octokit = github.getOctokit(secretToken);
-    const result = await octokit.rest.repos.createDispatchEvent({
+    await octokit.rest.repos.createDispatchEvent({
       owner: 'lottie-animation-community',
       repo: 'tests',
       event_type: 'trigger-tests',
       client_payload: {},
     });
-    console.log('result', result);
   } catch (error) {
     console.log('RUN ERROR: ', error);
   }
