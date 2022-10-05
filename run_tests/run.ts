93b34cab66e8b3250ba7428fcda58100f91f497c
airnan <hernantorrisi@gmail.com>
2022-10-04 20:09:40 -0400

    add test token

--- run_tests/run.ts
+++ run_tests/run.ts
@@ -6,8 +6,10 @@ async function run() {
     console.log('Running test');
     // const token = 'a';
     const secretToken = process.env.GITHUB_TOKEN || 'none secret';
+    const testToken = process.env.TEST_GITHUB_TOKEN || 'none2 secret';
     console.log('secretToken', secretToken.length);
-    const octokit = github.getOctokit(secretToken);
+    console.log('testToken', testToken.length);
+    const octokit = github.getOctokit(testToken);
     console.log(typeof octokit);
     const result = await octokit.rest.repos.createDispatchEvent({
       owner: 'lottie-animation-community',
