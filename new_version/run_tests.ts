917f25bb19724a520484b1107589d501b15165b4
airnan <hernantorrisi@gmail.com>
2022-10-05 09:26:31 -0400

    pass player to action

---
+++ new_version/run_tests.ts
@@ -0,0 +1,36 @@
+import * as github from '@actions/github';
+import yargs from 'yargs';
+
+function getPayload(player) {
+  if (player === 'lottie-web' || player === 'skottie') {
+    return {
+      player,
+    };
+  }
+}
+
+async function run() {
+  try {
+    console.log('Running test');
+    const argv = yargs(process.argv).argv;
+    const secretToken = process.env.BODYMOVIN_PERSONAL_TOKEN || 'none secret';
+    const octokit = github.getOctokit(secretToken);
+    const payload = getPayload(argv.player);
+    if (payload) {
+      await octokit.rest.repos.createDispatchEvent({
+        owner: 'lottie-animation-community',
+        repo: 'tests',
+        event_type: 'trigger-tests',
+        client_payload: {},
+      });
+    }
+  } catch (error) {
+    console.log('RUN ERROR: ', error);
+  }
+}
+
+try {
+  run();
+} catch (error) {
+  console.log('FAILED', error);
+}
